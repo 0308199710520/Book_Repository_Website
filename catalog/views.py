edc58a6818dcae63fb2fc3e7ad40098a1a95d39d
Michael <michaelpoole10@gmail.com>
2021-01-05 20:41:38 +0000

    New Pages

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 # Create your views here.
+
+def index(request):
+    """View function for the home page of the site"""
+
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
+    num_authors = Author.objects.all().count()
+    num_genres = Genre.objects.all().count()
+
+
+    context = {
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+        "num_genres": num_genres,
+    }
+
+    return render(request, "Index.html", context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = "my_book_list"
+    queryset = Book.objects.filter(title__contains="war")
+    template_name = "books"
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), "../Credentials.env")
+load_dotenv(dotenv_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,8 +85,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         "HOST": "127.0.0.1",
         'NAME': 'django_project',
-        "USER": "root",
-        "PASSWORD": "",
+        "USER": os.getenv("DATABASE_USERNAME"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
         "PORT":"3306",
         "OPTIONS":
             {
